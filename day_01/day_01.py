39101a1833a03adc0fede6086ef884282bd256af
Cihangir <ca01185@surrey.ac.uk>
2025-12-14 23:12:10 +0000

    First commit! - Advent of Code Day 1 Part 1 completed

---
+++ day_01/day_01.py
@@ -0,0 +1,25 @@
+# Advent of Code - Day 1
+sp = 50
+result = 0
+
+with open('day_01/test.txt') as f:
+    for line in f:
+        line = line.strip()
+
+        if line == '':
+            continue
+        
+        direction = line[0]
+        distance = int(line[1:])
+
+        if direction == 'R':
+            sp += distance
+        elif direction == 'L':
+            sp -= distance
+
+        sp = sp % 100
+
+        if sp == 0:
+            result += 1
+
+    print(result)
