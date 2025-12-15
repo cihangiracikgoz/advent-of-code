ee19738eeb7be36d05224ad9a1491e7b55bd3504
Cihangir <cihangiracikgoz01@gmail.com>
2025-12-15 01:50:03 +0000

    Day 1 Part 1 comments added for the solution

--- day_01/part_01.py
+++ day_01/part_01.py
@@ -1,4 +1,5 @@
 # Advent of Code - Day 1 Part 1
+
 sp = 50
 result = 0
 
@@ -8,17 +9,20 @@ with open('day_01/test.txt') as f:
 
         if line == '':
             continue
-        
+
         direction = line[0]
         distance = int(line[1:])
 
+        # Update starting position
         if direction == 'R':
             sp += distance
         elif direction == 'L':
             sp -= distance
 
+        # Normalize starting position within 0-99
         sp = sp % 100
 
+        # When starting position is exactly 0
         if sp == 0:
             result += 1
 
