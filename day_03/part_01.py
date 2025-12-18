5856d20fd75aa2f6d2f396e072ebba78eeae9e4e
Cihangir Acikgoz <cihangiracikgoz01@gmail.com>
2025-12-18 22:37:36 +0000

    Advent of Code Day 3 Part 1 completed

---
+++ day_03/part_01.py
@@ -0,0 +1,24 @@
+# Advent of Code - Day 3 - Part 1
+
+total = 0
+
+with open("day_03/input.txt") as f:
+    for line in f:
+        line = line.strip()
+        
+        if line == '':
+            continue
+
+        max_jolt = 0
+        
+        for i in range(len(line)):
+            for j in range(i + 1, len(line)):
+                jolt = int(line[i] + line[j])
+                max_jolt = max(max_jolt, jolt)
+
+        total += max_jolt
+
+print(total)
+
+
+            
