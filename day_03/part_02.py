003371d661af25578df6c312541b70c2f4a3d5fc
Cihangir Acikgoz <cihangiracikgoz01@gmail.com>
2025-12-20 03:18:12 +0000

    Advent of Code Day 3 Part 2 completed

---
+++ day_03/part_02.py
@@ -0,0 +1,24 @@
+# Advent of Code - Day 3 - Part 2
+
+total = 0
+num = 12
+
+with open("day_03/input.txt") as f:
+    for line in f:
+        line = line.strip()
+        
+        if line == '':
+            continue
+
+        stack = []
+        remove = len(line) - num
+
+        for i in line:
+            while remove > 0 and stack and stack[-1] < i:
+                stack.pop()
+                remove -= 1
+            stack.append(i)
+
+        leftmost = stack[:num]
+        jolt = int(''.join(leftmost))
+        total += jolt
